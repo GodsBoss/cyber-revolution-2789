ab6c890cd33a84a0e1c6c68468a43d5c3130aa0a
Timo Reitz <godsboss@gmx.de>
2022-04-02 12:04:52 +0200

    Include a very basic queue.

--- pkg/game/data_sprites.go
+++ pkg/game/data_sprites.go
@@ -2,8 +2,8 @@ package game
 
 var dataSprites = map[string]SpriteInfo{
 	"background": {
-		X: 1509,
-		Y: 41,
+		X: 1494,
+		Y: 44,
 		W: 320,
 		H: 200,
 	},
@@ -18,5 +18,17 @@ var dataSprites = map[string]SpriteInfo{
 		Y: 58,
 		W: 61,
 		H: 19,
+	},
+	"person_player": {
+		X: 443,
+		Y: 31,
+		W: 32,
+		H: 48,
+	},
+	"person_green_alien": {
+		X: 443,
+		Y: 79,
+		W: 32,
+		H: 48,
 	},
 }
--- pkg/game/state_playing.go
+++ pkg/game/state_playing.go
@@ -1,6 +1,8 @@
 package game
 
 import (
+	"math"
+
 	"github.com/GodsBoss/gggg/pkg/interaction"
 	"github.com/GodsBoss/gggg/pkg/rendering/canvas2drendering"
 )
@@ -9,11 +11,32 @@ const statePlayingID = "playing"
 
 type statePlaying struct {
 	spriteFactory *spriteFactory
+
+	personQueue *personQueue
 }
 
-func (state *statePlaying) init() {}
+func (state *statePlaying) init() {
+	state.personQueue = &personQueue{
+		persons: []person{
+			{
+				Type: personTypeGreenAlien,
+				x:    100,
+			},
+			{
+				Type: personTypePlayer,
+				x:    140,
+			},
+			{
+				Type: personTypeGreenAlien,
+				x:    180,
+			},
+		},
+	}
+	state.personQueue.calculateDesiredX()
+}
 
 func (state *statePlaying) tick(ms int) (next string) {
+	state.personQueue.Tick(ms)
 	return ""
 }
 
@@ -26,5 +49,68 @@ func (state *statePlaying) receiveMouseEvent(event interaction.MouseEvent) (next
 }
 
 func (state *statePlaying) renderable() canvas2drendering.Renderable {
-	return background
+	renderables := canvas2drendering.Renderables{
+		state.spriteFactory.create("background", 0, 0, 0),
+	}
+	renderables = append(renderables, state.persons()...)
+
+	return renderables
 }
+
+func (state *statePlaying) persons() canvas2drendering.Renderables {
+	renderables := make(canvas2drendering.Renderables, len(state.personQueue.persons))
+	for i, person := range state.personQueue.persons {
+		renderables[i] = state.spriteFactory.create("person_"+person.Type, int(person.x), personRenderY, 0)
+	}
+	return renderables
+}
+
+type personQueue struct {
+	persons []person
+}
+
+func (queue *personQueue) calculateDesiredX() {
+	l := len(queue.persons)
+	for i := range queue.persons {
+		queue.persons[i].desiredX = float64(personMostRightX - personHorizontalDistance*(l-i-1))
+	}
+}
+
+func (queue *personQueue) Tick(ms int) {
+	for i := range queue.persons {
+		queue.persons[i].Tick(ms)
+	}
+}
+
+type person struct {
+	Type string
+
+	x        float64
+	desiredX float64
+}
+
+func (p *person) Tick(ms int) {
+	speed := personSpeed * (float64(ms) / 1000)
+	if math.Abs(p.x-p.desiredX) <= speed {
+		p.x = p.desiredX
+		return
+	}
+	if p.desiredX < p.x {
+		speed = -speed
+	}
+	p.x += speed
+}
+
+const (
+	personTypePlayer     = "player"
+	personTypeGreenAlien = "green_alien"
+
+	personRenderY            = 80
+	personHorizontalDistance = 40
+
+	// personMostRightX is the x position the most right person desires.
+	personMostRightX = 260
+
+	// personSpeed is the speed of a person in pixel per second.
+	personSpeed = 25
+)
